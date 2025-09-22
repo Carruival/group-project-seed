db1efe9d0cdf64a429b5baa3ca2c438056134253
Carruival <carruival@alum.us.es>
2025-09-22 18:26:55 +0200

    cambios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import tokenService from "./services/token.service";
 import UserListAdmin from "./admin/users/UserListAdmin";
 import UserEditAdmin from "./admin/users/UserEditAdmin";
 import SwaggerDocs from "./public/swagger";
-
+import DeveloperList from "./developers";
 function ErrorFallback({ error, resetErrorBoundary }) {
   return (
     <div role="alert">
@@ -46,7 +46,8 @@ function App() {
       adminRoutes = (
         <>
           <Route path="/users" exact={true} element={<PrivateRoute><UserListAdmin /></PrivateRoute>} />
-          <Route path="/users/:username" exact={true} element={<PrivateRoute><UserEditAdmin /></PrivateRoute>} />          
+          <Route path="/users/:username" exact={true} element={<PrivateRoute><UserEditAdmin /></PrivateRoute>} />   
+          <Route path="/developers" element={<DeveloperList />} />       
         </>)
     }
     if (role === "PLAYER") {
